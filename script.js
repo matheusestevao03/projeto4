4eaa9ef8d84eb4f9f6fd9ccbb9f90b685fc6637a
Matheus <matheusestevao0309@gmail.com>
2024-04-01 09:45:40 -0300

    colocando elementos novos denovo

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ let num2 = parseInt(document.getElementById('num2').value) ;
 document.getElementById('resultado').innerHTML = num1 * num2;
 
 }
+function Inserir(){
+
+
+let numero = document.getElementById("posicao").value -1;
+let novo = document.getElementById("novo").value;
+document.getElementsByClassName("time")[numero].innerHTML = novo
+
+
+}
